17a25ad1f653125124c70dcf1f2f1e43a8bbd6f0
Rahman Elsirafy <rahman2408@gmail.com>
2021-05-26 06:34:00 -0400

    Adds a form that allows user to create a new task and sucessfully links the active user as the task owner as well as adds it to the database. Also edits some css layouts (I can't help myself, it's addicting)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,23 @@ class TasksController < ApplicationController
         erb :"/tasks/index"
     end
 
-    
+    get '/tasks/new' do
+        erb :"/tasks/new"
+    end
+
+    post '/tasks/new' do 
+        
+        new_task = Task.create(name: params[:name], description: params[:description], notes: params[:notes])
+        new_task.user_id = current_user.id
+        redirect "/tasks/#{new_task.id}"
+    end
+
+    get '/tasks/:id/edit' do 
+        @task = Task.find(params[:id])
+        erb :"/tasks/edit"
+    end
+
+
 
     get '/tasks/:id' do
         @task = Task.find(params[:id])
