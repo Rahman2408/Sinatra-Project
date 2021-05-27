0f5cc3408375a98fb8fd209abdd6e64485f63c67
Rahman Elsirafy <rahman2408@gmail.com>
2021-05-26 23:48:45 -0400

    Attempted to remove case sensitivity for username validation. Didn't work... Possibly incompatible with SQLite3? (best guess).

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_161943) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "notes"
-    t.integer "user_id"
-    t.boolean "completed", default: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
